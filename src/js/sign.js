455d07fb2e8d7b4f60414590f759fc726e838e01
Liza <liza.yemelyanova@list.ru>
2023-06-23 22:55:29 +0300

    sign.js mod

--- src/js/sign.js
+++ src/js/sign.js
@@ -1,12 +1,3 @@
-// const header = new URL(document.location).searchParams,
-//       name = document.
-
-// if (header.get("sign") !== "404") {
-//   info.innerHTML += `${header.get("info")}`;
-// } else if (header.get("info") === "404") {
-//   info.innerHTML = "Ошибка 404! Данной страницы не существует";
-// }
-
 const header = new URL(document.location).searchParams,
   sign_name = document.getElementById("sign_name"),
   sign_date = document.getElementById("sign_date"),
