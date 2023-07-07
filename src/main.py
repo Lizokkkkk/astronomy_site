576d438754070b79891ceea04096697a3aa93141
Diana-Zakhryapina <124994705+Diana-Zakhryapina@users.noreply.github.com>
2023-07-07 19:14:14 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request
 from monthly_horoscope_generation import *
-# import divination_by_tarot_by_month
+from divination_by_tarot_by_month import *
 
 app = Flask(__name__)
 
@@ -8,6 +8,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template("index.html")
+
 
 @app.route('/horoscope/')
 def horoscope():
@@ -22,21 +23,36 @@ def horoscope():
     third = forecast[3] + '.'
     return render_template("horoscope.html", first=first, second=second, third=third)
 
+
+@app.route('/tarot/')
+def tarot():
+    sign = request.args.get('sign')
+    tarot_output = divination_by_tarot(sign)
+    card_name_1 = tarot_output[0]['name']
+    card_name_2 = tarot_output[1]['name']
+    card_name_3 = tarot_output[2]['name']
+    card_image_1 = tarot_output[0]['image']
+    card_image_2 = tarot_output[1]['image']
+    card_image_3 = tarot_output[2]['image']
+    card_interpretation_1 = tarot_output[0]['interpretation']
+    card_interpretation_2 = tarot_output[1]['interpretation']
+    card_interpretation_3 = tarot_output[2]['interpretation']
+    return render_template("tarot.html", card_name_1=card_name_1, card_name_2=card_name_2, card_name_3=card_name_3,
+                           card_image_1=card_image_1, card_image_2=card_image_2, card_image_3=card_image_3,
+                           card_interpretation_1=card_interpretation_1, card_interpretation_2=card_interpretation_2,
+                           card_interpretation_3=card_interpretation_3)
+
+
 @app.route('/sign/')
 def sign():
     sign = request.args.get('sign')
     return render_template("sign.html")
 
+
 @app.route('/planet/')
 def planet():
     sign = request.args.get('sign')
     return render_template("planet.html")
-
-@app.route('/tarot/')
-def tarot():
-    sign = request.args.get('sign')
-    # tarot_output = divination_by_tarot_by_month.get_tarot(sign)
-    return render_template("tarot.html")
 
 
 if __name__ == '__main__':
